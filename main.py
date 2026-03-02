1cafb69a896e7664512650765fd96bbacc504071
Ynot <150277533+arsYakov@users.noreply.github.com>
2026-03-01 20:04:31 -0500

    we have a triangle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from logger import log_state
 from constants import *
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: \
@@ -10,13 +11,18 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
